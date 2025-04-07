2a701dbc4b4f9bd3f2ff833d5a3209256fae1469
katealtonwilliams <kate@altonwilliams.co.uk>
2025-04-07 16:37:51 +0100

    Adding s3 key name

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 resource "aws_lambda_function" "terraform_lambda_func" {
   s3_bucket     = "terraform-state-bucket-aebb0499259047588b07a8b0382e8026"
-  s3_key        = ""
+  s3_key        = "lambda_src.zip"
   function_name = "first-lambda-function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "hello_world.lambda_handler"
