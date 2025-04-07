03e8ca7b6be1e7696eecd6e4fd60b1ae7f2afe38
katealtonwilliams <kate@altonwilliams.co.uk>
2025-04-07 12:29:03 +0100

    Defining where to put the terraform state lock

---
+++ backend.tf
@@ -0,0 +1,17 @@
+# maybe put this into dynamo like on cs?
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state"
+    region  = "eu-west-2"
+    key     = "s3-github-actions/terraform.tfstate"
+    encrypt = true
+  }
+    required_version = ">=0.13.0"
+    required_providers {
+      aws = {
+        version = ">= 2.7.0"
+        source = "hashicorp/aws"
+      }
+    }
+}
+
