eaf055bb90be62066c0f7cd1a4dd348cbb5391a1
katealtonwilliams <kate@altonwilliams.co.uk>
2025-04-07 12:27:00 +0100

    Updating terraform to use in pipeline

--- main.tf
+++ main.tf
@@ -1,33 +1,28 @@
 provider "aws" {
     region = "eu-west-2"
-    shared_credentials_files = ["/home/kate/.aws/credentials"]
 }
 
 resource "aws_iam_role" "lambda_role" {
     name = "terraform_aws_lambda_role"
-    assume_role_policy = <<EOF
-{
+    assume_role_policy = jsonencode({
   "Version": "2012-10-17",
   "Statement": [
     {
       "Effect": "Allow",
-      "Sid": "",
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
   ]
-}
-EOF
+})
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
     name        = "aws_iam_policy_for_terraform_aws_lambda_role"
     path        = "/"
     description = "AWS IAM policy for managing aws lambda role"
-    policy      = <<EOF
-    {
+    policy      = jsondecode({
       "Version": "2012-10-17",
       "Statement": [
        {
@@ -40,8 +35,7 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
         "Effect": "Allow"
        }
     ]
-}
-EOF
+})
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
@@ -49,14 +43,9 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
-data "archive_file" "zip_the_python_code" {
-  type = "zip"
-  source_dir  = "${path.module}/python_backend/lambdas"
-  output_path = "${path.module}/python_backend/lambdas/hello_word.zip"
-}
-
 resource "aws_lambda_function" "terraform_lambda_func" {
-  filename      = "${path.module}/python_backend/lambdas/hello_word.zip"
+  s3_bucket     = "lambda-artifacts-bucket"
+  s3_key        = "lambda_src.zip"
   function_name = "first-lambda-function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "hello_world.lambda_handler"
